15b426c9ea3a6dbad6cab41a2ecf5d232372d450
fesanchez <jfesanchez@gmail.com>
2019-05-03 16:27:37 -0400

    Create block_exalib.php

---
+++ lang/block_exalib.php
@@ -0,0 +1,111 @@
+<?php
+// This file is part of Exabis Library
+//
+// (c) 2016 GTN - Global Training Network GmbH <office@gtn-solutions.com>
+//
+// Exabis Library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This script is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You can find the GNU General Public License at <http://www.gnu.org/licenses/>.
+//
+// This copyright notice MUST APPEAR in all copies of the script!
+$string['pluginname'] = 'Exabis Biblioteca';
+$string['heading'] = 'Exabis Biblioteca';
+$string['exalib:use'] = 'Use Exalib: Viendo y Buscando';
+$string['exalib:open'] = 'Abrir archivos en Biblioteca';
+$string['exalib:creator'] = 'Crear contenido y gestionar contenido propio';
+$string['exalib:admin'] = 'Administrar todo el contenido';
+$string['block/exalib:open'] = 'Abrir elementos y archivos';
+$string['exalib:addinstance'] = 'Añadir instancia al curso';
+// tabs
+$string['tab_library'] = 'Entradas';
+$string['tab_mine'] = 'Mis entradas';
+$string['tab_manage_content'] = 'Administrar Contenido de Bibloteca';
+$string['tab_review'] = 'Revisar Items';
+$string['tab_manage_cats'] = 'Administrar Categorias';
+$string['tab_manage_reviewers'] = 'Revisores';
+$string['tab_fachsprachliches_lexikon'] = 'Indice';
+$string['tab_course_settings'] = 'Seteo';
+// admin-actions-inc
+$string['edit_content'] = 'Administrar el contenido de la biblioteca';
+$string['edit'] = 'Editar';
+$string['delete'] = 'Borrar';
+$string['addcat'] = 'Agregar subcategoria';
+$string['name'] = 'Nombre';
+$string['parent'] = 'Pariente';
+$string['source'] = 'Fuente';
+$string['abstract'] = 'Abstract';
+$string['content'] = 'Contenido';
+$string['previmg'] = 'Previsualización Imagen';
+$string['linktitle'] = 'Enlace al Titulo';
+$string['link'] = 'Link';
+$string['files'] = 'Archivo';
+$string['authors'] = 'Autores';
+$string['onlineset'] = 'Online Seteo';
+$string['onlinefrom'] = 'Formulario Online';
+$string['onlineto'] = 'Online para';
+$string['online'] = 'Online';
+$string['offline'] = 'Offline';
+$string['categories'] = 'Categorias';
+$string['groups'] = 'Grupos';
+$string['addmaincat'] = 'agregar categoria madre';
+$string['deletecat'] = 'borrar categoria {$a}?';
+$string['availableusers'] = 'Usuarios Disponibles';
+$string['showall'] = 'Mostrar todo';
+// adv-search
+$string['welcome'] = 'Bienvenido a la Bibloteca Exabis';
+$string['all'] = 'Todo';
+$string['abstracts'] = 'Abstracts';
+$string['documents'] = 'Documentos';
+$string['images'] = 'Imagenes';
+$string['podcasts'] = 'Podcasts';
+$string['webcasts'] = 'Webcasts';
+$string['searchby'] = 'Buscar by: ';
+$string['title'] = 'Titulo';
+$string['author'] = 'Autor';
+$string['search'] = 'Buscar';
+$string['filter'] = 'Filtro: ';
+$string['applyfilter'] = 'Aplicar Filtros';
+$string['noitemsfound'] = 'Sin Itemes';
+// detail
+$string['itemnotfound'] = 'item no encontrados';
+$string['background'] = 'Fondo';
+$string['methods'] = 'Metodos';
+$string['results'] = 'Resultados';
+$string['conclusion'] = 'Conclusión';
+$string['back'] = 'Atrás';
+$string['terms_of_use'] = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.';
+// index
+$string['administration'] = 'Administración';
+$string['inthiscat'] = 'En esta categoria';
+$string['wholelib'] = 'Biblioteca entera';
+$string['latest'] = 'Últimos cambios';
+$string['newentry'] = 'Agregar una nueva entrada';
+// lib
+$string['notallowed'] = 'No tienes permiso para ver el contenido de la biblioteca';
+$string['nocreator'] = 'Usted no es Creator Exalib';
+$string['noadmin'] = 'Usted no es Exalib Admin';
+$string['noedit'] = 'No tienes permiso para editar este artículo';
+$string['download'] = 'Descargar';
+$string['created'] = 'Creado';
+$string['by_person'] = 'por {$a}';
+$string['maincat'] = 'Categoria Superior';
+$string['subcat'] = 'Sub Categoría';
+$string['delete_confirmation'] = '¿Realmente quieres borrar? "{$a}"?';
+$string['comment_delete_confirmation'] = '¿De verdad quieres borrar este comentario?';
+$string['alt_tab_library'] = 'Entradas';
+$string['alt_tab_mine'] = 'Mis Entradas';
+$string['search_all'] = 'Buscar en todas las entradas';
+$string['alt_search_all'] = 'Buscar en todas las entradas';
+$string['all_entries'] = 'Todas las entradas';
+$string['alt_all_entries'] = 'Todas las entradas';
+$string['alt_heading'] = 'Bibloteca Exabis';
+$string['alt_tab_manage_content'] = 'Administrar contenido biblotecario';
+$string['alt_tab_review'] = 'Items revisados';
